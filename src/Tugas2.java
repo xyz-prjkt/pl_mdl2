fd09d08aba2cb235a8e4f39e0d489f47698ec239
nauzyx <yuandy7gamers@gmail.com>
2022-10-18 00:40:19 +0700

    PL: Modul2: Implement interface for Tugas2

--- src/Tugas2.java
+++ src/Tugas2.java
@@ -1,13 +1,19 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Tugas2 {
+interface library {
+    void cekDate(int date);
+    void cekFrom(String from);
+    void cekName(String name);
+    void list();
+}
+public class Tugas2 implements library {
     Scanner input = new Scanner(System.in);
     String inName, inFrom;
     int inDate, inIndex = 1;
     public String[] book = new String[1];
     String[] place = {"Malang", "Bandung", "Surabaya"};
-    void cekDate(int date){
+    public void cekDate(int date){
         for (int i = 0; i < book.length; i++) {
             int parsedYear = Integer.parseInt(parseBook(book[i], "date"));
             if (parsedYear < 2018 && date < 2018) {
@@ -18,7 +24,7 @@ public class Tugas2 {
         }
     }
 
-    private void cekFrom(String from){
+    public void cekFrom(String from){
         for (int i = 0; i < book.length; i++) {
             if (parseBook(book[i], "from").equals(from)){
                 System.out.printf("%s, %s. %s\n", parseBook(book[i],"name"), parseBook(book[i], "from"), parseBook(book[i], "date"));
@@ -26,7 +32,7 @@ public class Tugas2 {
         }
     }
 
-    void cekName(String name){
+    public void cekName(String name){
         for (int i = 0; i < book.length; i++) {
             if (parseBook(book[i], "name").equals(name)){
                 System.out.printf("%s, %s. %s\n", parseBook(book[i],"name"), parseBook(book[i], "from"), parseBook(book[i], "date"));
@@ -34,7 +40,7 @@ public class Tugas2 {
         }
     }
 
-    void list(){
+    public void list(){
         System.out.printf("%-15s | %-15s | %-15s", "Name", "From", "Year");
         for (String s : book) {
             System.out.printf("\n%-15s , %-15s . %-15s", parseBook(s,"name"), parseBook(s, "from"), parseBook(s, "date"));
